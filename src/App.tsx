1121dec8cfca31f2dbf778513a0410ffc74b4499
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2021-04-16 08:37:02 +0200

    Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './styles/index.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Text changed
         </p>
         <a
           className="App-link"
@@ -22,5 +22,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
