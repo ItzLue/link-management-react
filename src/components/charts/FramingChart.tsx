43ad7e87ac4a3d3c87bc2c8c14a59ce889d650b3
ClaraML96 <63050283+ClaraML96@users.noreply.github.com>
2021-06-08 11:08:22 +0200

    bar chart

--- src/components/charts/FramingChart.tsx
+++ src/components/charts/FramingChart.tsx
@@ -1,31 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { IFramingData } from '../../types/api/data';
-import { Doughnut } from 'react-chartjs-2';
+import React, {useEffect, useState} from 'react';
+import { Bar } from 'react-chartjs-2';
+import {IFramingData} from "../../types/api/data";
+import dayjs from "dayjs";
 
 type IProps = { framingData: IFramingData[] };
 
 const FramingChart: React.FC<IProps> = ({framingData }) => {
 
+    const [labels, setLabels] = useState<string[]>([]);
+
+    const makeDateData = () => {
+        setInterval(() => {
+            const date = dayjs(Date.now()).format('HH:mm:ss').toString();
+            setLabels([...labels, date]);
+        }, 10000);
+    };
+
+    useEffect(() => {
+        makeDateData();
+    }, [labels, setLabels]);
+
     const data = {
-        labels: ['Errors corrected', 'Errors detected'],
+        labels,
         datasets: [
             {
-                label: 'Errors detected',
-                data: [framingData[0].errorsCorrected,framingData[0].errorsDetected],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                ],
-                borderWidth: 1,
+                label: 'Eroors detected',
+                data: framingData.map((d) => d.errorsDetected),
+                backgroundColor: 'rgb(255, 99, 132)',
+            },
+            {
+                label: 'Errors corrected',
+                data: framingData.map((d) => d.errorsCorrected),
+                backgroundColor: 'rgb(54, 162, 235)',
             },
         ],
     };
 
-    return <Doughnut type='doughnut' data={data}/>
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+            xAxes: [
+                {
+                    stacked: true,
+                },
+            ],
+        },
+    };
+
+
+    return <Bar data={data} options={options} type='bar'/>
 };
 
 export default FramingChart;
