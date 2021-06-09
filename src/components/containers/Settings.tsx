c319f9b8c2a13f74344a043fae83c793bf209871
ClaraML96 <63050283+ClaraML96@users.noreply.github.com>
2021-06-09 13:52:39 +0200

    failedToCompile

--- src/components/containers/Settings.tsx
+++ src/components/containers/Settings.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Disclosure } from '@headlessui/react';
-import { IEncryptionData, IFramingData, IVideoData } from '../../types/api/data';
-import { HiArrowNarrowDown } from 'react-icons/all';
-import { FaHistory } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
 
-type IProps = { videoData?: IVideoData[]; framingData: IFramingData[]; encryptionData: IEncryptionData[] };
+const Settings: React.FC = () => {
+	const { register, handleSubmit, formState: { errors } } = useForm();
+	const onSubmit = data => console.log(data);
+	console.log(errors);
 
-const Settings: React.FC<IProps> = ({ }) => {
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input type="text" placeholder="Shared timestamp" {...register("Shared timestamp", {})} />
+			<input type="number" placeholder="Encryption process time" {...register("Encryption process time", {})} />
+			<input type="text" placeholder="Encryption type" {...register("Encryption type", {})} />
+			<input type="number" placeholder="Encryption encrypted" {...register("Encryption encrypted", {})} />
+			<input type="number" placeholder="Framing process time" {...register("Framing process time", {})} />
+			<input type="number" placeholder="Framing errors detected" {...register("Framing errors detected", {})} />
+			<input type="number" placeholder="Framing errors corrected" {...register("Framing errors corrected", {})} />
+			<input type="number" placeholder="Framing error det rate" {...register("Framing error det rate", {})} />
+			<input type="number" placeholder="Framing error corr rate" {...register("Framing error corr rate", {})} />
+			<input type="number" placeholder="Video max package size" {...register("Video max package size", {})} />
+			<input type="number" placeholder="Video min package size" {...register("Video min package size", {})} />
+			<input type="text" placeholder="Video protocol version" {...register("Video protocol version", {})} />
+			<input type="text" placeholder="Video protocol" {...register("Video protocol", {})} />
+			<input type="number" placeholder="Video bit rate" {...register("Video bit rate", {})} />
+			<input type="number" placeholder="Video ping" {...register("Video ping", {})} />
+			<input type="number" placeholder="Link process time" {...register("Link process time", {})} />
+			<input type="number" placeholder="Link video packets received" {...register("Link video packets received", {})} />
+			<input type="number" placeholder="Link video delay" {...register("Link video delay", {})} />
+			<input type="number" placeholder="Link framing errors detected" {...register("Link framing errors detected", {})} />
+			<input type="number" placeholder="Link framing errors corrected" {...register("Link framing errors corrected", {})} />
 
+			<input type="submit" />
+		</form>
+	);
 }
 export default Settings;
