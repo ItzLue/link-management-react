165f96e012a5e938d125b12f5ba93a38fd905fdf
ClaraML96 <63050283+ClaraML96@users.noreply.github.com>
2021-06-11 12:18:34 +0200

    sdsa

--- src/components/containers/ErrorMessage.tsx
+++ src/components/containers/ErrorMessage.tsx
@@ -4,9 +4,9 @@ import {HiExclamationCircle} from "react-icons/all";
 const ErrorMessage: React.FC = () => {
 	return (
 		<div>
-			<div className='w-full px-4 p-20'>
+			<div className='w-full px-4 p-10'>
 				<h1 className='text-center text-2xl'>
-					Connection to server failed <HiExclamationCircle/>
+					<HiExclamationCircle className='mr-2 inline'/> Connection to server failed
 				</h1>
 			</div>
 		</div>
