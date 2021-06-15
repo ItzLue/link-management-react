d97e89bd1379d190b71bfebd600195618590cf99
ClaraML96 <63050283+ClaraML96@users.noreply.github.com>
2021-06-15 10:12:03 +0200

    navi

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -6,7 +6,7 @@ type IProps = { menuItems: IMenuItem[] };
 
 const Navigation: React.FC<IProps> = ({ menuItems }) => {
 	return (
-		<nav className='w-full fixed bottom-0 h-12 shadow-md border border-gray-100 z-10 bg-white'>
+		<nav className='w-full fixed bottom-0 h-20 shadow-md border border-gray-100 z-10 bg-white'>
 			<div className='justify-between place-content-center flex flex-row h-full items-center px-12'>
 				{menuItems.map((item, key) => (
 					<Link to={item.link} key={key}>
