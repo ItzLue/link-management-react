dc19c3920a5aa8257a72362b638b297122f8de80
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2021-06-17 22:28:21 +0200

    Update Navigation.tsx

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction, Chip } from '@material-ui/core';
 import { HiAdjustments, HiHome } from 'react-icons/hi';
 import { FaHistory } from 'react-icons/fa';
@@ -7,8 +7,16 @@ import Switch from '@material-ui/core/Switch';
 
 type IProps = { onToggle: () => void; isSimStarted: boolean; isRealTransmission: boolean };
 const Navigation: React.FC<IProps> = ({ onToggle, isSimStarted, isRealTransmission }) => {
-	const [value, setValue] = useState('recents');
+	const [value, setValue] = useState('home');
+	const history = useHistory();
 	const handleChange = (event: React.ChangeEvent<{}>, newValue: any) => setValue(newValue);
+
+	useEffect(() => {
+		if (!isSimStarted) {
+			history.push('/');
+			setValue('home');
+		}
+	}, [isSimStarted]);
 
 	const toggle = () => (
 		<div className='inline-flex items-center mb-2'>
